d8107f72ecf021c3ccb8a3633cce0b24d37a34fe
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-08-10 17:48:50 -0300

    Testbench ALU 74181 logic gates declared

---
+++ eight-bit-alu/tb_ula_74181.sv
@@ -0,0 +1,21 @@
+module tb_ula_74181;
+
+logic [3:0] a, b, s;
+logic m, c_in;
+
+logic [3:0] f;
+logic a_eq_b, c_out;
+
+ula_74181 dut
+(
+    .a(a),
+    .b(b),
+    .s(s),
+    .m(m),
+    .c_in(c_in),
+    .f(f),
+    .a_eq_b(a_eq_b),
+    .c_out(c_out),
+);
+
+endmodule
