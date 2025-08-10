bc28212b1d233146d4f8db8312eda88252503925
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-08-10 19:43:09 -0300

    Testbench ALU 74181 logic implemented

--- eight-bit-alu/tb/tb_ula_74181.sv
+++ eight-bit-alu/tb/tb_ula_74181.sv
@@ -18,4 +18,32 @@ ula_74181 dut
     .c_out(c_out)
 );
 
+initial
+    begin
+        $dumpfile("ula_74181.vcd");
+        $dumpvars(0, tb_ula_74181);
+
+        $display("Inicio da simulacao");
+
+        for (int mode = 1'b0; mode <= 1'b1; mode++)
+            begin
+                m = mode;
+
+                for (int selection = 4'b0000; selection <= 4'b1111; selection++)
+                    begin
+                        s = selection;
+                        a = 4'b0100;
+                        b = 4'b0011;
+                        c_in = 1'b1;
+
+                        #10;
+
+                        $display("M: %b, S: %0d, A: %0d, B: %0d, C_IN: %b, F: %0d, C_OUT: %b", m, s, a, b, c_in, f, c_out);
+                    end
+            end
+
+        $display("Fim da simulacao");
+
+        $finish;
+    end
 endmodule
