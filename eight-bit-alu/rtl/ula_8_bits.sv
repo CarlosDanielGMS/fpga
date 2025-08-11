08d0a6215a6fd4f631c6aa75273764db8daaaae0
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-08-10 23:33:05 -0300

    ALU 8 bits logic implemented

--- eight-bit-alu/rtl/ula_8_bits.sv
+++ eight-bit-alu/rtl/ula_8_bits.sv
@@ -9,3 +9,28 @@ module ula_8_bits (
 
 logic [3:0] f_highest, f_lowest;
 logic c_mid;
+
+ula_74181 ula_highest (
+    .a(a[7:4]),
+    .b(b[7:4]),
+    .s(s),
+    .m(m),
+    .c_in(c_mid),
+    .f(f_highest),
+    .a_eq_b(),
+    .c_out(c_out)
+);
+
+ula_74181 ula_lowest (
+    .a(a[3:0]),
+    .b(b[3:0]),
+    .s(s), .m(m),
+    .c_in(c_in),
+    .f(f_lowest),
+    .a_eq_b(),
+    .c_out(c_mid)
+);
+
+assign f = {f_highest, f_lowest};
+assign a_eq_b = (a == b);
+endmodule
