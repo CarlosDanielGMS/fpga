ef86ac14ccc8f8804c1dad5fb25e4130326365b0
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-11-02 16:01:13 -0300

    [LoRa communication] Receiver display configured

--- lora-communication/receiver/receiver.c
+++ lora-communication/receiver/receiver.c
@@ -5,6 +5,7 @@
 #include "hardware/i2c.h"
 #include "hardware/pio.h"
 #include "hardware/uart.h"
+#include "include/ssd1306.h"
 
 #define IN 0
 #define OUT 1
@@ -12,6 +13,8 @@
 #define NOT_PRESSED 1
 
 #define RESET_BUTTON_PIN 5
+#define DISPLAY_SDA_PIN 14
+#define DISPLAY_SCL_PIN 15
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -33,9 +36,17 @@
 
 bool resetButtonStatus = NOT_PRESSED;
 
+struct render_area frame_area = {
+    start_column : 0,
+    end_column : ssd1306_width - 1,
+    start_page : 0,
+    end_page : ssd1306_n_pages - 1
+};
+
 void initializeComponents();
 void readButtons();
 void resetIntoBootselMode();
+void setDisplay(char *message);
 
 void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
     blink_program_init(pio, sm, offset, pin);
@@ -129,6 +140,15 @@ void initializeComponents()
     gpio_init(RESET_BUTTON_PIN);
     gpio_set_dir(RESET_BUTTON_PIN, IN);
     gpio_pull_up(RESET_BUTTON_PIN);
+    
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(DISPLAY_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(DISPLAY_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(DISPLAY_SDA_PIN);
+    gpio_pull_up(DISPLAY_SCL_PIN);
+    ssd1306_init();
+    calculate_render_area_buffer_length(&frame_area);
+    setDisplay("Inicializando");
 }
 
 void readButtons()
@@ -139,4 +159,14 @@ void readButtons()
 void resetIntoBootselMode()
 {
     reset_usb_boot(0, 0);
+}
+
+void setDisplay(char *message)
+{
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    
+    ssd1306_draw_string(ssd, 0, 8, message);
+    
+    render_on_display(ssd, &frame_area);
 }
